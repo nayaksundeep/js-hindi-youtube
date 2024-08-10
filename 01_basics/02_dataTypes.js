bf71ccc504a547b34d1de157b4a8c2682f7d1d75
Sundeep <98259005+nayaksundeep@users.noreply.github.com>
2024-08-10 20:12:57 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all JS code as newer version
+
+//allert (3+3) // we are using nodejs, not browser
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+
+//number => 2 to power 53
+//bigint
+//string
+//boolean => true / false
+//null => standalone value
+//undefined =>
+// symobol = unique 
+
+
+
+//object
+
+console.log(typeof "sundeep");
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
