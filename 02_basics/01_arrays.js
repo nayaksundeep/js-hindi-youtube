34b15bea1f81ebf59aeeda6cd1f61340c6910e08
Sundeep <98259005+nayaksundeep@users.noreply.github.com>
2024-08-14 22:54:14 +0000

    arrays basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,43 @@
+//array
+
+const myArr = [0,1,2,3,4,5]
+const HerosArr = ["shaktiman", "captain America"]
+
+
+const myArr2 = new Array(1,2,3,4,5)
+
+console.log(myArr[0]);
+
+//Array Methods
+
+myArr.push(6)
+myArr.pop() 
+
+myArr.unshift(9)
+myArr.shift()
+
+console.log(myArr.includes(9))
+
+
+const newArr = myArr.join()
+console.log(myArr); 
+
+console.log(typeof newArr)
+
+
+//slice,splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1)
+
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("c",myArr);
+console.log(myn2);
+
+
+
